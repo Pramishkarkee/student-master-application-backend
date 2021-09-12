0936894cf7112c40da6bef5b2bc2628f56846406
nawarazpokhrel <pokhrelnawaraz@yahoo.com>
2021-09-12 16:16:18 +0545

    feat(proj):  fix env settings and email settings

--- config/settings/local.py
+++ config/settings/local.py
@@ -26,8 +26,18 @@ CACHES = {
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 EMAIL_BACKEND = env(
-    "DJANGO_EMAIL_BACKEND", default="django.core.mail.backends.console.EmailBackend"
+    "DJANGO_EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend"
 )
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_USE_TLS = True
+# EMAIL_PORT = 465
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+DEFAULT_FROM_EMAIL = ''
+
 
 # django-debug-toolbar
 # ------------------------------------------------------------------------------
